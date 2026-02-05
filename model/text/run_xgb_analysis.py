5a64c42e3eb6e8f9dd65c3b48dcc3ac85dd9275d
techie-ranith <ranith2k3@gmail.com>
2026-02-05 12:43:30 +0530

    data set update

--- model/text/run_xgb_analysis.py
+++ model/text/run_xgb_analysis.py
@@ -66,7 +66,7 @@ print("\n" + "=" * 60)
 print("Splitting data (80/20 train/test)...")
 print("=" * 60)
 
-X_train, X_test, y_train, y_test = train_test_split(
+X_train, X_test, y_train, y_test = train_test_split(    
     X, y, test_size=0.2, random_state=42, stratify=y
 )
 
